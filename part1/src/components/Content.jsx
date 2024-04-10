d6ec236a1218abd9952fc6e5ee22ad235d427792
Adam <adamwasylak1@gmail.com>
2024-04-10 14:10:10 +0200

    Exercise 1.3 done

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,11 +1,11 @@
 import Part from "./Part"
 
-const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
+const Content = ({ name1, name2, name3, exercises1, exercises2, exercises3 }) => {
 	return (
 		<>
-		<Part part={part1} exercises={exercises1}></Part>
-		<Part part={part2} exercises={exercises2}></Part>
-		<Part part={part3} exercises={exercises3}></Part>
+		<Part name={name1} exercises={exercises1}></Part>
+		<Part name={name2} exercises={exercises2}></Part>
+		<Part name={name3} exercises={exercises3}></Part>
 		</>
 	)
 }
