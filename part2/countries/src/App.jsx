f878d3e34f0d94d3787b8dc122730d6abd236161
Adam <adamwasylak1@gmail.com>
2024-04-21 20:53:19 +0200

    exercise 2.18 done

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const App = () => {
+	const [countries, setCountries] = useState([])
+	const [search, setSearch] = useState('')
+
+	useEffect(() => {
+		fetch('https://restcountries.com/v3.1/all')
+			.then(response => response.json())
+			.then(data => setCountries(data))
+	}, [])
+
+	const filteredCountries = countries.filter(country =>
+		country.name.common.toLowerCase().includes(search.toLowerCase())
+	)
+	console.log(filteredCountries)
+
+	const searchCountry = event => {
+		event.preventDefault()
+		setSearch(event.target.value)
+	}
+
+	let content
+
+	if (filteredCountries.length === 1) {
+		content = filteredCountries.map(country => (
+			<div key={country.name.common}>
+				<h1>{country.name.common}</h1>
+				<p>capital {country.capital}</p>
+				<p>population {country.population}</p>
+				<h2>languages</h2>
+				{Object.values(country.languages).map(language => (
+					<li key={language}>{language}</li>
+				))}
+				<img src={country.flags.png} alt={country.name.common} width='200' height='100' />
+			</div>
+		))
+	} else if (filteredCountries.length > 10) {
+		content = <p>Too many matches, specify another filter</p>
+	} else if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+		content = filteredCountries.map(country => <p key={country.name.common}>{country.name.common}</p>)
+	} else {
+		content = <p>No matches, specify another filter</p>
+	}
+
+	return (
+		<>
+			<div>
+				<p>
+					find countries <input type='text' value={search} onChange={searchCountry} />
+				</p>
+				{content}
+			</div>
+		</>
+	)
+}
+
+export default App
