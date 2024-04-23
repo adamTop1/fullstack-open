21833b63eab3e492211c2612a97d3fbe4729f6c0
Adam <adamwasylak1@gmail.com>
2024-04-23 09:30:44 +0200

    exercise 3.6 done

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,26 +2,26 @@ const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
 ]
 
 const PORT = 3001
@@ -29,42 +29,51 @@ const PORT = 3001
 app.use(express.json())
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+	res.json(persons)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+	const id = Number(req.params.id)
+	const person = persons.find(person => person.id === id)
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+	const id = Number(req.params.id)
+	persons = persons.filter(person => person.id !== id)
+	res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
- const body = req.body
- const personObj = {
-    id: Math.floor(Math.random() * 1000),
-    name: body.name,
-    number: body.number
+	const body = req.body
+	if (!body.name || !body.number) {
+		return res.status(400).json({
+			error: 'name or number missing',
+		})
+	} else if (persons.find(person => person.name === body.name || person.number === body.number)) {
+		return res.status(400).json({
+			error: 'name or number must be unique',
+		})
+	} else {
+    const personObj = {
+      id: Math.floor(Math.random() * 1000),
+      name: body.name,
+      number: body.number,
+    }
+    persons = persons.concat(personObj)
+    res.json(personObj.name + ' added to phonebook')
   }
-  persons = persons.concat(personObj)
-  res.json(personObj.name + ' added to phonebook')
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${date}</p>`)
+	const date = new Date()
+	res.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${date}</p>`)
 })
 
-
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:3001/')
+	console.log('Server running on http://localhost:3001/')
 })
