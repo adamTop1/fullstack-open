2fa1c84d346112f5626eaf5a0b3345bd0add49f9
Adam <adamwasylak1@gmail.com>
2024-05-13 16:46:04 +0200

    exercise 4.10 done

--- part4/bloglist/backend/tests/blogs_api.test.js
+++ part4/bloglist/backend/tests/blogs_api.test.js
@@ -14,13 +14,34 @@ test('blogs are returned as json', async () => {
 })
 
 test('blog has id property', async () => {
-   const response = await api.get('/api/blogs')
-    const blogId = response.body[0].id
+	const response = await api.get('/api/blogs')
+	const blogId = response.body[0].id
 
-    assert.strictEqual(blogId, '6639fc65f3cb0dd8c1096c0a')
+	assert.strictEqual(blogId, '6639fc65f3cb0dd8c1096c0a')
 })
 
+test('create a new blog', async () => {
 
+    const blogs = await api.get('/api/blogs')
+    const initialBlogsLength = blogs.body.length
+
+	const newBlog = {
+		author: 'tester',
+		url: 'https://reactpatterns.com/',
+		likes: 3,
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+    
+    const blogsAtEnd = await api.get('/api/blogs')
+    const blogsLength = blogsAtEnd.body.length
+
+    assert.strictEqual(blogsLength, initialBlogsLength + 1)
+})
 
 after(async () => {
 	await mongoose.connection.close()
