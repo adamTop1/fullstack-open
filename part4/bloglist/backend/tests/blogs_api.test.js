e708742b0b8978a094cc552a02dfe31069b3dc33
Adam <adamwasylak1@gmail.com>
2024-05-13 16:35:06 +0200

    exercise 4.9 done

--- part4/bloglist/backend/tests/blogs_api.test.js
+++ part4/bloglist/backend/tests/blogs_api.test.js
@@ -2,16 +2,26 @@ const { test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const assert = require('node:assert')
 
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+	await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
 })
 
+test('blog has id property', async () => {
+   const response = await api.get('/api/blogs')
+    const blogId = response.body[0].id
+
+    assert.strictEqual(blogId, '6639fc65f3cb0dd8c1096c0a')
+})
+
+
+
 after(async () => {
-  await mongoose.connection.close()
+	await mongoose.connection.close()
 })
