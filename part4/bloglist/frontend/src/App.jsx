ff454e5d572b0378ea7e2fba40f2036fb87837fc
Adam <adamwasylak1@gmail.com>
2024-05-20 13:50:39 +0200

    exercise 5.1 done

---
+++ part4/bloglist/frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  return (
+    <div>
+      <h2>blogs</h2>
+     {user === null ? <LoginForm setUser={setUser} /> : <p>{user.name} logged in</p>}
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part4/bloglist/frontend/src/components/LoginForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import loginService from "../services/loginService"
+
+const LoginForm = ({setUser}) => {
+    const [username, setUsername] = useState('') 
+    const [password, setPassword] = useState('') 
+
+    // {
+    //     "username": "root",
+    //     "name": "Superuser",
+    //     "password": "salainen"
+    // }
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        console.log('logging in with', username, password)
+        
+        try {
+            const user = await loginService.login({ username, password})
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+	return (
+		<div>
+			<h2>Log in to application</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username 
+					<input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+				</div>
+				<div>
+					password 
+					<input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	)
+}
+
+export default LoginForm
