c4acdbab4ee8ea5fbd6b59cc1b358023418345d9
Adam <adamwasylak1@gmail.com>
2024-05-22 18:01:37 +0200

    exercise 5.7 done

--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,19 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from './Toggable'
+
+const Blog = ({ blog }) => {
+	console.log(blog)
+
+	return (
+		<div className='blog'>
+			<div className='blog-title'>title: {blog.title}</div>
+			<Togglable buttonLabel='show more info'>
+				<div>author: {blog.author}</div>
+				<div>url: {blog.url}</div>
+				<div>likes :{blog.likes} <button>like</button></div>
+				<div>username:{blog.user && blog.user.username}</div>
+			</Togglable>
+		</div>
+	)
+}
 
 export default Blog
